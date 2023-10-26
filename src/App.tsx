1337f7aefabe932a850eae2ed3a4cc0a10ddb15f
HyerimNac:/Users/나혜림/OneDrive/바탕 화면/test/venv/Scripts/python.exe <rimhyena@gmail.com>
2023-10-26 16:22:11 +0900

    ✨feat: 폴더&페이지 업뎃

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MyPage from "./pages/MyPage";
 import Main from "./pages/Main";
 import CartPage from "./pages/CartPage";
+import SearchPage from "./pages/SearchPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import PaymentPage from "./pages/PaymentPage";
+import ListPage from "./pages/ListPage";
+import DetailPage from "./pages/DetailPage";
+import Bookmark from "./components/Main/Bookmark";
+import BookmarkPage from "./pages/BookmarkPage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -11,6 +20,14 @@ const App = () => {
           <Route path="/" element={<Main />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="/bookmark" element={<BookmarkPage />} />
+          <Route path="/notfound" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
