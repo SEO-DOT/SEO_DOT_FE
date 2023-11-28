11887c337f936221584770af34338e69c00ccd58
2Zerozero <hawgae@gmail.com>
2023-11-28 12:33:58 +0900

    [feat] 메인페이지 비주얼 페이지네이션 버튼 수정

--- src/components/Main/Visual.tsx
+++ src/components/Main/Visual.tsx
@@ -12,14 +12,14 @@ const Visual = ({ slides }: { slides: any[] }) => {
   return (
     <div className="mx-6">
       <Swiper
-        className="mt-[28px] h-[280px]"
+        className="mt-[28px] h-[300px]"
         modules={[Pagination]}
         slidesPerView={1.3268} // 한 번에 보여줄 슬라이드 수
         spaceBetween={14}
         pagination={{ clickable: true }}
       >
         {slides.map((slide: SlidesType, index: number) => (
-          <SwiperSlide className="h-[243px]" key={index}>
+          <SwiperSlide className="h-[242px]" key={index}>
             <img
               className="object-cover h-full rounded-bl-[30px]"
               src={slide.url}
