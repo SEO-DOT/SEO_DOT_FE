4b7bf5659957190553929f2a46e9f3cb3f09c7f2
2Zerozero <hawgae@gmail.com>
2023-12-06 02:53:40 +0900

    [feat] 비주얼 슬라이드 수정

--- src/components/Main/Visual.tsx
+++ src/components/Main/Visual.tsx
@@ -3,12 +3,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import "swiper/css/pagination";
 import "./styles.css";
+import Visual1 from "../../assets/images/main/Visual_1.png";
+import Visual2 from "../../assets/images/main/Visual_2.png";
+import Visual3 from "../../assets/images/main/Visual_3.png";
+import Visual4 from "../../assets/images/main/Visual_4.png";
 
-interface SlidesType {
-  url: string;
-}
-
-const Visual = ({ slides }: { slides: any[] }) => {
+const Visual = () => {
+  const slides = [Visual1, Visual2, Visual3, Visual4];
   return (
     <div className="mx-6">
       <Swiper
@@ -18,11 +19,11 @@ const Visual = ({ slides }: { slides: any[] }) => {
         spaceBetween={14}
         pagination={{ clickable: true }}
       >
-        {slides.map((slide: SlidesType, index: number) => (
+        {slides.map((slide: any, index: number) => (
           <SwiperSlide className="h-[242px]" key={index}>
             <img
               className="object-cover h-full rounded-bl-[30px]"
-              src={slide.url}
+              src={slide}
               alt="사진"
             />
           </SwiperSlide>
